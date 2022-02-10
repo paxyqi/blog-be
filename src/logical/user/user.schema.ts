a44516411da4a572dca41498c5a45ff1d3bff901
Pixy Qi <pixy.qi@gmail.com>
2022-02-10 15:56:36 +0800

    删除冗余代码

--- src/logical/user/user.schema.ts
+++
@@ -1,9 +0,0 @@
-// user.schema.ts
-import * as mongoose from 'mongoose';
-
-export const userSchema = new mongoose.Schema({
-  _id: { type: String, required: true }, // 覆盖 Mongoose 生成的默认 _id
-  user_name: { type: String, required: true },
-  administrator: { type: Boolean, required: true },
-  password: { type: String, required: true },
-});
